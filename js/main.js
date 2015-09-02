80cecc2feaca9e9c47f49ee177b3b348d788ae00
jaredwerba <jwerba@gmail.com>
2015-09-02 17:20:37 -0400

    chef site
    bootstrap, jQuery, js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+var main = function() {
+  $('.btn-slides').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+$(".menu1").click(function(){
+  $(this).toggleClass("open");
+});
+
+$(document).ready(main);
